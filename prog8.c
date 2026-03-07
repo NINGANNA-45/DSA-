83f12c8daeeee36b80416bddb572f4143cb7cc7b
NINGANNA SHIRASHYAD <shirashyadpundalik@gmail.com>
2026-03-07 19:06:28 +0530

    prog8.c

---
+++ prog8.c
@@ -0,0 +1,14 @@
+//longest substring without repeating characters //incomplete
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num,i,count=0;
+    char words[100];
+    printf("Enter the number of words:");
+    scanf("%d",&num);
+    printf("Enter the words:");
+    scanf("%s",&words);
+    for(i=0;i<num;i++)
+   
